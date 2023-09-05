6f326af8acfb182bd71a36a82e9e4d3af3e126a4
RolinFirelink <99341107+RolinFirelink@users.noreply.github.com>
2023-09-05 12:01:53 +0800

    Update SMSUtils.java

--- src/main/java/com/rolin/pigsale/utils/SMSUtils.java
+++ src/main/java/com/rolin/pigsale/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou","LTAI5tMKpCBFJKNLhAnNiPPu", "QngcHleoCWvIczT0EWqZhKpvzDgwGa");
+		
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
